49081179c013491532e5bef7e9e40fa72ef8910a
Your Name <root@localhost>
2017-12-13 17:15:36 +0200

    arguments

--- nginx/bin/run.sh
+++ nginx/bin/run.sh
@@ -5,10 +5,13 @@ hostname=$1
 port=$2
 
 #check if there are
-if [[ $# -eq 0 ]] || [[ $# -gt 2 ]]; then
+if [[ $# -eq 0 ]]; then
  #Start nginx if there is noting to wait for
  echo "Starting nginx."
  nginx -g 'daemon off;'
+elif [[ $# -eq 1 ]] || [[ $# -gt 2 ]]; then
+    echo "There is an error in your arguments."
+    echo "Use $(basename $0) <hostname> <port>"
 else
   while [[ $result -ne 0 ]]; do
     nc -zv $hostname $port
