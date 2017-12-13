d7fb2cb4997d00ee055c71510300a7a533fc0833
Your Name <root@localhost>
2017-12-13 16:57:04 +0200

    2nd commit

--- nginx/bin/run.sh
+++ nginx/bin/run.sh
@@ -4,12 +4,19 @@ result=1
 hostname=$1
 port=$2
 
-while [[ $result -ne 0 ]]; do
+#check if there are
+if [[ $# -eq 0 ]] || [[ $# -gt 2 ]]; then
+ #Start nginx if there is noting to wait for
+ echo "Starting nginx."
+ nginx -g 'daemon off;'
+else
+  while [[ $result -ne 0 ]]; do
     nc -zv $hostname $port
     result=$?
     sleep 2
     echo "Waiting for $hostname to start"
-done
+  done
 
-echo "$hostname can be reached on port $port."
-nginx -g 'daemon off;'
+  echo "$hostname can be reached on port $port. Starting nginx."
+  nginx -g 'daemon off;'
+fi
